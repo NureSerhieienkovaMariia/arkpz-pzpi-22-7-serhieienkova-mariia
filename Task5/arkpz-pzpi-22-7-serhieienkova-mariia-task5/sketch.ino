c04fd369c97e7da5c43cf8493bb9d156875f18db
NureSerhieienkovaMariia <mariia.serhieienkova@nure.ua>
2025-01-02 13:57:46 +0200

    Update sketch.ino

--- Task5/arkpz-pzpi-22-7-serhieienkova-mariia-task5/sketch.ino
+++ Task5/arkpz-pzpi-22-7-serhieienkova-mariia-task5/sketch.ino
@@ -3,44 +3,44 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
-// Настройки датчика температуры
-#define ONE_WIRE_BUS 23 // Пин для DS18B20
+// Налаштування датчика температури
+#define ONE_WIRE_BUS 23 // Пін для DS18B20
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
-// Pulse Generator Settings
-#define PULSE_PIN 2            // Pulse generator pin (GPIO2)
-#define SAMPLING_INTERVAL 1000 // Интервал для выборки пульса
+// Налаштування генератора пульсу
+#define PULSE_PIN 2            // Пін генератора пульсу (GPIO2)
+#define SAMPLING_INTERVAL 1000 // Інтервал для вибірки пульсу (в мілісекундах)
 volatile uint16_t pulse = 0;
 uint16_t count = 0;
 int heartRate = 0;
-unsigned long lastPulseTime = 0; // Переменная для отслеживания времени последнего измерения пульса
+unsigned long lastPulseTime = 0;
 
-// WiFi настройки
-const char* ssid = "Wokwi-GUEST"; // Укажите ваш SSID
-const char* password = ""; // Укажите ваш пароль
+// Налаштування WiFi
+const char* ssid = "Wokwi-GUEST"; // Ім'я мережі WiFi
+const char* password = "";
 
-// MQTT настройки
-const char* mqtt_server = "broker.hivemq.com"; // Адрес брокера
-const int mqtt_port = 1883; // Порт MQTT
-const char* mqtt_topic = "agewell/data"; // Тема для публикации данных
-const char* mqtt_user = ""; // Пользователь MQTT (если есть)
-const char* mqtt_password = ""; // Пароль MQTT (если есть)
+// Налаштування MQTT
+const char* mqtt_server = "broker.hivemq.com"; // Адреса брокера MQTT
+const int mqtt_port = 1883;                   // Порт MQTT
+const char* mqtt_topic = "agewell/data";      // Тема для публікації даних
+const char* mqtt_user = "";
+const char* mqtt_password = "";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-// Пороговые значения для температуры и давления
-const float temperatureThresholdHigh = 41.0;
-const float temperatureThresholdLow = 28.0; 
-const float pressureThresholdHigh = 180.0;
-const float pressureThresholdLow = 110.0;
+// Порогові значення для температури та тиску
+const float temperatureThresholdHigh = 41.0;  // Максимальна температура
+const float temperatureThresholdLow = 28.0;   // Мінімальна температура
+const float pressureThresholdHigh = 180.0;    // Максимальний тиск
+const float pressureThresholdLow = 110.0;     // Мінімальний тиск
 
-const int deviceId = 1;
+const int deviceId = 1; // Ідентифікатор пристрою
 
-// Функция для обработки импульсов пульса
+// Функція для обробки імпульсів пульсу
 void HeartRateInterrupt() {
-  pulse++;  // Увеличиваем счетчик пульса при каждом импульсе
+  pulse++;  // Збільшуємо лічильник пульсу при кожному імпульсі
 }
 
 void connectToWiFi() {
@@ -67,20 +67,20 @@ void connectToMQTT() {
 }
 
 void setup() {
-  // Инициализация последовательного порта
+  // Ініціалізація серійного порту
   Serial.begin(115200);
 
-  // Инициализация датчика температуры
+  // Ініціалізація датчика температури
   sensors.begin();
 
-  // Инициализация датчика пульса
+  // Ініціалізація генератора пульсу
   pinMode(PULSE_PIN, INPUT);
-  attachInterrupt(digitalPinToInterrupt(PULSE_PIN), HeartRateInterrupt, RISING); // Настройка прерывания на пин PULSE_PIN
+  attachInterrupt(digitalPinToInterrupt(PULSE_PIN), HeartRateInterrupt, RISING); // Налаштування переривання на пін PULSE_PIN
 
-  // Подключение к WiFi
+  // Підключення до WiFi
   connectToWiFi();
 
-  // Настройка MQTT
+  // Налаштування MQTT
   client.setServer(mqtt_server, mqtt_port);
   connectToMQTT();
 
@@ -93,47 +93,47 @@ void loop() {
   }
   client.loop();
 
-  // Считывание температуры
+  // Зчитування температури
   sensors.requestTemperatures();
   float temperature = sensors.getTempCByIndex(0);
 
-  // Измерение пульса за 1 секунду
+  // Вимірювання пульсу за 1 секунду
   unsigned long currentMillis = millis();
   if (currentMillis - lastPulseTime >= SAMPLING_INTERVAL) {
-    heartRate = pulse * 60;  // Умножаем количество импульсов за 1 секунду на 60, чтобы получить количество пульсов в минуту
-    pulse = 0;  // Сбрасываем счетчик пульсов
-    lastPulseTime = currentMillis;  // Обновляем время
+    heartRate = pulse * 60;  // Перетворюємо кількість імпульсів за 1 секунду в кількість ударів за хвилину
+    pulse = 0;              // Скидаємо лічильник імпульсів
+    lastPulseTime = currentMillis; // Оновлюємо час
 
-    // Генерация данных для систолического и диастолического давления
-    int systolicPressure = random(110, 180);  // Генерация случайного систолического давления
-    int diastolicPressure = random(70, 120);  // Генерация случайного диастолического давления
+    // Генерація даних для систолічного та діастолічного тиску
+    int systolicPressure = random(110, 180);  // Генерація випадкового систолічного тиску
+    int diastolicPressure = random(70, 120);  // Генерація випадкового діастолічного тиску
 
     String healthStatus = "Normal";
-    
-    // Проверка состояния по температуре
+
+    // Перевірка стану за температурою
     if (temperature >= temperatureThresholdHigh) {
       healthStatus = "Patient in critical condition due to high temperature!";
     } else if (temperature <= temperatureThresholdLow) {
       healthStatus = "Patient in critical condition due to low temperature!";
     }
 
-    // Проверка состояния по давлению
+    // Перевірка стану за тиском
     if (systolicPressure >= pressureThresholdHigh || diastolicPressure >= pressureThresholdLow) {
-    if (healthStatus == "Normal") {
-      healthStatus = "Patient in critical condition due to pressure!";
-    } else {
-      healthStatus += " And critical condition due to pressure!";
+      if (healthStatus == "Normal") {
+        healthStatus = "Patient in critical condition due to pressure!";
+      } else {
+        healthStatus += " And critical condition due to pressure!";
+      }
     }
-}
 
-    // Создание JSON строки для отправки
-    String payload = "{\"temperature\": " + String(temperature) + 
-                      ", \"systolic_blood_pressure\":" + String(systolicPressure) + 
+    // Створення JSON-рядка для відправлення
+    String payload = "{\"temperature\": " + String(temperature) +
+                      ", \"systolic_blood_pressure\":" + String(systolicPressure) +
                       ",\"distolic_blood_pressure\":" + String(diastolicPressure) +
-                      ", \"pulse\": " + String(heartRate) + 
+                      ", \"pulse\": " + String(heartRate) +
                       ", \"device_id\": " + String(deviceId) + "}";
 
-    // Публикация данных в MQTT
+    // Публікація даних у MQTT
     if (temperature != DEVICE_DISCONNECTED_C) {
       client.publish(mqtt_topic, payload.c_str());
       Serial.println("Published to MQTT: " + payload);
@@ -143,6 +143,6 @@ void loop() {
     }
   }
 
-  // Задержка в 5 секунд
+  // Затримка на 5 секунд
   delay(5000);
 }
